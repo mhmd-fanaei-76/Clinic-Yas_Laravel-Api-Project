5a3718344a639f66270a16e9e5f943acc200c463
mohammad <mohammdfanaei031@gmail.com>
2024-02-01 16:28:55 +0330

    Add Fillable And Relation In Turn Model

--- app/Models/Turn.php
+++ app/Models/Turn.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Turn extends Model
 {
     use HasFactory;
+    protected $fillable = ['patient_id','time_id','turns_date','confirmation'];
+
+    public function times()
+    {
+        return $this->belongsTo(Time::class,'time_id','id');
+    }
+
+    public function users()
+    {
+        return $this->belongsTo(User::class,'patient_id','id');
+    }
+
 }
