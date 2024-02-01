845794515bae61987d4a4580ff2da6493acd4b4b
mohammad <mohammdfanaei031@gmail.com>
2024-02-01 15:49:49 +0330

    Add Fillable And Relation In Section Model

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Section extends Model
 {
     use HasFactory;
+    protected $fillable = ['section_name'];
+
+    public function times()
+    {
+        return $this->hasMany(Time::class, 'section_id', 'id');
+    }
+
+    public function users()
+    {
+        return $this->hasMany(User::class, 'section_id', 'id');
+    }
 }
