54a7d51f1781cadce98bded68dac329480a21d31
mohammad <mohammdfanaei031@gmail.com>
2024-02-01 16:03:36 +0330

    Add Fillable And Relation In Model Time

--- app/Models/Time.php
+++ app/Models/Time.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Time extends Model
 {
     use HasFactory;
+    protected $fillable = ['doctor_id','section_id','day','start_hours','end_hours','confirmation'];
+    public function scopeConfirmed($query)
+    {
+        return $query->where('confirmation',true);
+    }
+
+    public function users()
+    {
+        return $this->belongsTo(User::class,'doctor_id','id');
+    }
+
+    public function sections()
+    {
+        return $this->belongsTo(Section::class,'section_id','id');
+    }
+
+    public function turns()
+    {
+        return $this->hasMany(Turn::class,'time_id','id');
+    }
 }
